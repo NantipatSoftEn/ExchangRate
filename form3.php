a92fba35b86fa9a9bf4e7046fdd37a942bcaa9d9
ARM\nantipat <keymasterviriya1150@gmail.com>
2017-05-19 14:20:18 +0700

    update report month

--- form3.php
+++ form3.php
@@ -11,41 +11,28 @@
 		<article class="tile is-child notification is-info">
 			<p class="title"></p>
 			<?php
-			function DateThai($strDate)
-			{
-				$m = null;
-				if($strDate =='01')
-					$m = "January";
-				else if($strDate =='02')
-					$m = "February";
-				else if($strDate =='03')
-					$m = "March";
-				else if($strDate =='04')
-					$m = "April";
-				else if($strDate =='05')
-					$m = "May";
-				else if($strDate =='06')
-					$m = "June";
-				else if($strDate =='07')
-					$m = "July";
-				else if($strDate =='08')
-					$m = "August";
-				else if($strDate =='09')
-					$m = "September";
-				else if($strDate =='10')
-					$m = "October";
-				else if($strDate =='11')
-					$m = "November";
-				else if($strDate =='12')
-					$m = "December";
-				return $m;
-			}
+			
 
 			date_default_timezone_set("asia/bangkok");
 			echo'<p align = "right"><font size = "2"> วันที่ออกรายงาน '.date('Y-m-d').' </font></p><br>';
 			echo" <h1><U><center>บริษัท เพียวนา จำกัด</U></center></h1>";
 			echo"<center>รายงานรายได้ประจำเดือน</center></br>";
-			echo "รายได้ประจำเดือนที่:	" . DateThai(date('m')) . "</br></br>";
+			?>
+			<form action="page.php" method="post" name="frmMain">
+			<span class="select">
+			<select name="lmName1" OnChange="window.location='?item='+this.value;">
+			<?php 
+
+			
+			$m=array('January','February','March','April','May','June','July','August','September','October','November','December');
+			$d=array('01','02','03','04','05','06','07','08','09','10','11','12');
+			echo "<option> Select Month</option>";
+			for ($i=0; $i < count($m) ; $i++) { 
+				echo "<option value=$d[$i]>$m[$i]</option>";
+			}
+			echo '</select>';
+			echo '</span>';
+			echo '</form>';
 			echo"<table class='table'>";
 			$head = array("CountryCode", "Quantity","Price","Date");
 			echo "<tr>";
@@ -53,52 +40,116 @@
 				echo "<td>" . $head[$i] . "</td>";
 			echo "</tr>";
 			$con = mysqli_connect("localhost", "root", "", "database");
-			$result = mysqli_query($con, "SELECT CountryCode,Money FROM money");
+			$result = mysqli_query($con, "SELECT Money FROM money");
 			$Money=array();
-			$CountryCode=array();
+			
 			while ($row = mysqli_fetch_array($result, MYSQLI_NUM))
 			{
-				$CountryCode[]=$row[0];
-				$Money[]=$row[1];
+				$Money[]=$row[0];
 			}
 
 			$con=mysqli_connect("localhost","root","","database2");
-			$result=mysqli_query($con,"SELECT CountryCode,Quanlity,date FROM data where MONTH(Date)=' ".date('m')." '");
-			$Rate_compare=array('','','','','','','','','','','','','');
+			$result=mysqli_query($con,"SELECT CountryCode,Quanlity,date FROM data where MONTH(Date)=' ".date($_GET["item"])." '");
+			$Rate_compare=array();
 			echo '<form name="form3" method="post" action="report3.php">';
+			$i=0;
 			while($row=mysqli_fetch_array($result,MYSQLI_NUM))
 			{
-	
-				
-				$Rate_compare[$row[0]]=array($row[1],$row[2]);
+
+				$Rate_compare[$i]=array($row[0],$row[1],$row[2]);
+				$i++;
 			}
-	
 
-			
-			
 			$sum_mouth=0;
-
-			for ($i=0; $i < count($CountryCode); $i++) { 
+			$Price=0;
+			$CountryCode=array('USD-50-100',
+				'USD-5-20',
+				'USD-1-2',
+				'GBP',
+				'EUR',
+				'JPY',
+				'HKD',
+				'MYR',
+				'SGD',
+				'BND',
+				'CNY',
+				'IDR',	
+				'INR',);
+			for ($i=0; $i < count($Rate_compare); $i++) { 
 				echo "<tr>";
-			
-				echo "<td>".$CountryCode[$i]."</td>";
-				echo "<td>".$Rate_compare[$CountryCode[$i]][0]."</td>";
-				echo "<td>".$Rate_compare[$CountryCode[$i]][0]*$Money[$i]."</td>";
-				echo "<td>".$Rate_compare[$CountryCode[$i]][1]."</td>";
+
+				echo "<td>".$Rate_compare[$i][0]."</td>";
+				echo "<td>".$Rate_compare[$i][1]."</td>";
+
+				if($Rate_compare[$i][0] == 'USD-50-100')
+				{
+					$Price=$Rate_compare[$i][1]*$Money[0];
+				}
+				else if($Rate_compare[$i][0] == 'USD-5-20')
+				{
+					$Price=$Rate_compare[$i][1]*$Money[1];
+				}
+				else if($Rate_compare[$i][0] == 'USD-1-2')
+				{
+					$Price=$Rate_compare[$i][1]*$Money[2];
+				}
+				else if($Rate_compare[$i][0] == 'GBP')
+				{
+					$Price=$Rate_compare[$i][1]*$Money[3];
+				}
+				else if($Rate_compare[$i][0] == 'EUR')
+				{
+					$Price=$Rate_compare[$i][1]*$Money[4];
+				}
+				else if($Rate_compare[$i][0] == 'JPY')
+				{
+					$Price=$Rate_compare[$i][1]*$Money[5];
+				}
+				else if($Rate_compare[$i][0] == 'HKD')
+				{
+					$Price=$Rate_compare[$i][1]*$Money[6];
+				}
+				else if($Rate_compare[$i][0] == 'MYR')
+				{
+					$Price=$Rate_compare[$i][1]*$Money[7];
+				}
+				else if($Rate_compare[$i][0] == 'SGD')
+				{
+					$Price=$Rate_compare[$i][1]*$Money[8];
+				}
+				else if($Rate_compare[$i][0] == 'BND')
+				{
+					$Price=$Rate_compare[$i][1]*$Money[9];
+				}
+				else if($Rate_compare[$i][0] == 'CNY')
+				{
+					$Price=$Rate_compare[$i][1]*$Money[10];
+				}
+				else if($Rate_compare[$i][0] == 'IDR')
+				{
+					$Price=$Rate_compare[$i][1]*$Money[11];
+				}
+				else if($Rate_compare[$i][0] == 'INR')
+				{
+					$Price=$Rate_compare[$i][1]*$Money[12];
+				}
+
+				echo "<td>".$Price."</td>";
+				echo "<td>".$Rate_compare[$i][2]."</td>";
 				echo "</tr>";
 
-				echo '<input type="hidden" name="CountryCode[]" value='.$CountryCode[$i].'>';
-				$sum_mouth+=$Rate_compare[$CountryCode[$i]][0]*$Money[$i];
+				echo '<input type="hidden" name="CountryCode[]" value='.$Rate_compare[$i][0].'>';
+				$sum_mouth+=$Price;
 
-				
-				echo '<input type="hidden" name="Quanlity[]" value='.$Rate_compare[$CountryCode[$i]][0].'>';
-				echo '<input type="hidden" name="Price[]" value='.$Rate_compare[$CountryCode[$i]][0]*$Money[$i].'>';
-				echo '<input type="hidden" name="date[]" value='.$Rate_compare[$CountryCode[$i]][1].'>';
-				
+
+				echo '<input type="hidden" name="Quanlity[]" value='.$Rate_compare[$i][1].'>';
+				echo '<input type="hidden" name="Price[]" value='.$Price.'>';
+				echo '<input type="hidden" name="date[]" value='.$Rate_compare[$i][2].'>';
+
 
 			}
 			echo "<td>Total :".$sum_mouth." Bath</td>";
-			echo '<input type="hidden" name="size" value='.count($CountryCode).'>';
+			echo '<input type="hidden" name="size" value='.count($Rate_compare).'>';
 			echo '<input type="hidden" name="summouth" value='.$sum_mouth.'>';
 			echo "</table>";
 
